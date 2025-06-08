649e27e7cb4996d34869dfac571f9adde68545fe
zhangyuxin <1679607834@qq.com>
2025-06-08 17:10:58 +0800

    解耦FFT实现：添加Cooley-Tukey接口切换实现

--- src/main/scala/FFT/Config.scala
+++ src/main/scala/FFT/Config.scala
@@ -1,13 +1,22 @@
-
 package FFT
+
+import org.scalacheck.Prop
 
 trait HasDataConfig {
   val DataWidth = 32
   val BinaryPoint = 16
 }
 
+// 定义FFT算法类型
+object FFTAlgorithm extends Enumeration {
+  type FFTAlgorithm = Value
+  val R2MDC, CooleyTukey = Value
+}
+
 trait HasElaborateConfig {
   val FFTLength = 512
   val useGauss = false
   val supportIFFT = false
+  // 新增：FFT算法选择，默认为R2MDC保证向后兼容
+  val fftAlgorithm = FFTAlgorithm.R2MDC
 }
